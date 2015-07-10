72fe5c7add489a69854d7302d888a41b3bdd9e24
Louis-Claude Canon <louis-claude.canon@univ-fcomte.fr>
2015-07-10 10:07:49 +0200

    Improve style and factor 'with'

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
 consumption <- read.csv("household_power_consumption.txt", sep=";",
-                        header=T,
-                        na.strings="?")
+                        header=T, na.strings="?")
 consumption$datetime <- strptime(paste(consumption$Date, consumption$Time),
                                  format="%d/%m/%Y %H:%M:%S")
 consumptionSelect <- subset(consumption,
@@ -8,8 +7,7 @@ consumptionSelect <- subset(consumption,
                             & datetime < "2007-02-03 00:00:00 CET")
 
 png("plot1.png")
-hist(consumptionSelect$Global_active_power,
-     col="red",
+hist(consumptionSelect$Global_active_power, col="red",
      main="Global Active Power",
      xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -9,17 +9,16 @@ consumptionSelect <- subset(consumption,
 
 png("plot4.png")
 par(mfrow=c(2, 2))
-with(consumptionSelect, plot(datetime, Global_active_power, type="l", xlab="",
-                             ylab="Global Active Power"))
-
-with(consumptionSelect, plot(datetime, Voltage, type="l"))
-
-with(consumptionSelect, plot(datetime, Sub_metering_1, type="l", xlab="",
-                             ylab="Energy sub metering"))
-with(consumptionSelect, lines(datetime, Sub_metering_2, col=2))
-with(consumptionSelect, lines(datetime, Sub_metering_3, col=4))
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lwd=1, col=c(1,2,4), bty="n")
-
-with(consumptionSelect, plot(datetime, Global_reactive_power, type="l"))
+with(consumptionSelect, {
+    plot(datetime, Global_active_power, type="l",
+         xlab="", ylab="Global Active Power")
+    plot(datetime, Voltage, type="l")
+    plot(datetime, Sub_metering_1, type="l",
+         xlab="", ylab="Energy sub metering")
+    lines(datetime, Sub_metering_2, col=2)
+    lines(datetime, Sub_metering_3, col=4)
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+           lwd=1, col=c(1,2,4), bty="n")
+    plot(datetime, Global_reactive_power, type="l")
+})
 dev.off()
